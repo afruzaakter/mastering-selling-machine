d21e9433860ad1bfff038979f730263777e8b6c7
razumolla <rajuahmed9344@gmail.com>
2024-09-23 12:36:32 +0600

    new

--- src/assets/Selling-Machine-Page/RegistrationForm.jsx
+++ src/assets/Selling-Machine-Page/RegistrationForm.jsx
@@ -1,85 +1,102 @@
-
-import { FaUserTie, FaPhoneVolume } from 'react-icons/fa';
+import { FaUserTie, FaPhoneVolume } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { GiGraduateCap } from "react-icons/gi";
-import Timer from './Timer';
-
+import Timer from "./Timer";
 
 const RegistrationForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.name.value);
+    console.log(e.target.phoneNumber.value);
+    console.log(e.target.email.value);
+    console.log(e.target.profession.value);
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        console.log(e.target.name.value);
-        console.log(e.target.phoneNumber.value);
-        console.log(e.target.email.value);
-        console.log(e.target.profession.value);
-    };
+  return (
+    <>
+      {/* -------------Registration form start ------------------ */}
+      <div className="card bg-base-100  lg:w-96  shadow-xl">
+        <div className="card-body lg:items-center lg:text-center">
+          <h2 className="lg:text-3xl text-2xl font-bold text-gray-600 ">
+            Join our Free Webinar
+          </h2>
+          <p className=" [font-size:18px] lg:mr-10 lg:ml-10">
+            Join our free webinar to learn how to get the best out of your
+            online marketing efforts
+          </p>
+          {/* -------------- Timer  Code start -------------- */}
 
-    return (
-        <>
-            {/* -------------Registration form start ------------------ */}
-            <div className="card bg-base-100  lg:w-96  shadow-xl">
+          <Timer />
 
-                <div className="card-body lg:items-center lg:text-center">
-                    <h2 className="lg:text-3xl text-2xl font-bold text-gray-600 ">Join our Free Webinar</h2>
-                    <p className=" [font-size:18px] lg:mr-10 lg:ml-10">Join our free webinar to learn how to get the best out of your online marketing efforts</p>
-                    {/* -------------- Timer  Code start -------------- */}
+          {/* -------------- Timer  Code end -------------- */}
 
-                    <Timer />
+          {/* -------------- Registration form  start  -------------- */}
 
-                    {/* -------------- Timer  Code end -------------- */}
+          <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+            {/* --------------- Name field ------------- */}
+            <label className="input input-bordered flex items-center gap-2">
+              <FaUserTie />
+              <input
+                type="text"
+                name="name"
+                className="grow"
+                placeholder="Your Name"
+              />
+            </label>
+            {/* --------------- Phone number field ------------- */}
+            <label className="input input-bordered flex items-center gap-2">
+              <FaPhoneVolume />
+              <input
+                type="text"
+                name="phoneNumber"
+                className="grow"
+                placeholder="Your Phone Number"
+              />
+            </label>
 
-                    {/* -------------- Registration form  start  -------------- */}
+            {/* --------------- Email field ------------- */}
+            <label className="input input-bordered flex items-center gap-2">
+              <MdEmail />
+              <input
+                type="email"
+                name="email"
+                className="grow"
+                placeholder="Your Email"
+              />
+            </label>
 
-                    <form className='flex flex-col gap-3' onSubmit={handleSubmit}>
-                        {/* --------------- Name field ------------- */}
-                        <label className="input input-bordered flex items-center gap-2">
-                            <FaUserTie />
-                            <input type="text" name="name" className="grow" placeholder="Your Name" />
-                        </label>
-                        {/* --------------- Phone number field ------------- */}
-                        <label className="input input-bordered flex items-center gap-2">
-                            <FaPhoneVolume />
-                            <input type="text" name="phoneNumber" className="grow" placeholder="Your Phone Number" />
-                        </label>
+            {/* ------------- Select field-------------------------- */}
+            <label className="input  input-bordered  flex items-center gap-4">
+              <GiGraduateCap />
+              <select name="profession" className="select w-full  ">
+                <option disabled selected>
+                  Select Your Profession
+                </option>
+                <option>Sales Professional</option>
+                <option>উদ্যোক্তা</option>
+                <option>Influencer</option>
+                <option>Others</option>
+              </select>
+            </label>
+            <label className="input  flex items-center gap-2">
+              <button className=" btn text-white btn-wide  bg-blue-800  ">
+                Register
+              </button>
+            </label>
+          </form>
 
-
-                        {/* --------------- Email field ------------- */}
-                        <label className="input input-bordered flex items-center gap-2">
-                            <MdEmail />
-                            <input type="email" name="email" className="grow" placeholder="Your Email" />
-                        </label>
-
-                        {/* ------------- Select field-------------------------- */}
-                        <label className="input  input-bordered  flex items-center gap-4">
-                            <GiGraduateCap />
-                            <select name="profession" className="select w-full  ">
-                                <option disabled selected>Select Your Profession</option>
-                                <option>Sales Professional</option>
-                                <option>উদ্যোক্তা</option>
-                                <option>Influencer</option>
-                                <option>Others</option>
-                            </select>
-                        </label>
-                        <label className="input  flex items-center gap-2">
-                            <button className=" btn text-white btn-wide  bg-blue-800  ">Register</button>
-                        </label>
-                    </form>
-
-
-
-                    {/* -------------- Registration form  end  -------------- */}
-                    <div className="card-actions">
-
-
-                        <p className='[font-size:16px] mt-4'>ওয়েবিনারটি কিছুক্ষনের মধ্যেই শুরু হতে যাচ্ছে, অংশগ্রহন করার জন্য এখনই ১০০% ফ্রি রেজিস্ট্রেশন সম্পন্ন করুন।</p>
-
-                    </div>
-                </div>
-            </div >
-            {/* -------------Registration form end ------------------ */}
-        </>
-    );
+          {/* -------------- Registration form  end  -------------- */}
+          <div className="card-actions">
+            <p className="[font-size:16px] mt-4">
+              ওয়েবিনারটি কিছুক্ষনের মধ্যেই শুরু হতে যাচ্ছে, অংশগ্রহন করার জন্য
+              এখনই ১০০% ফ্রি রেজিস্ট্রেশন সম্পন্ন করুন।
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* -------------Registration form end ------------------ */}
+    </>
+  );
 };
 
 export default RegistrationForm;
--- src/assets/Selling-Machine-Page/WebinarVideo.jsx
+++ src/assets/Selling-Machine-Page/WebinarVideo.jsx
@@ -1,29 +1,19 @@
-
-
 const WebinarVideo = () => {
-    return (
-        <div className="m-3">
-           
-       
+  return (
+    <div className="m-3">
+      <iframe
+        width="1200"
+        height="700"
+        src="https://www.youtube.com/embed/BImFWxvAjDc?autoplay=1&controls=0&loop=1&disablekb=1"
+        title="ওয়েবিনার এ রেজিঃ করার জন্য অভিনন্দন জানাই"
+        allow="accelerometer; autoplay=1; controls; disablekb=1; loop=1; modestbranding=1  "
+        allowfullscreen
+      ></iframe>
 
-            <iframe
-                width="1200"
-                height="700"
-                src="https://www.youtube.com/embed/BImFWxvAjDc?autoplay=1&controls=0&loop=1&disablekb=1"
-                title="ওয়েবিনার এ রেজিঃ করার জন্য অভিনন্দন জানাই"
-                allow="accelerometer; autoplay=1; controls; disablekb=1; loop=1; modestbranding=1  "
-                allowfullscreen
-            ></iframe>
-           
-            <p>সেলিং মেশিন তৈরি ও লাইফটাইম সাপোর্ট পেতে এনরোল নিন এখনই</p>
-            <button> Enroll Now & Get 97% Discount </button>
-        </div>
-    );
+      <p>সেলিং মেশিন তৈরি ও লাইফটাইম সাপোর্ট পেতে এনরোল নিন এখনই</p>
+      <button> Enroll Now & Get 97% Discount </button>
+    </div>
+  );
 };
 
 export default WebinarVideo;
-
-
-
-
-

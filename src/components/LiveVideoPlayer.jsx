b5244e771dc4e50906ffe0240b93aa99ad384401
razumolla <rajuahmed9344@gmail.com>
2024-10-07 16:01:40 +0600

    ss

--- src/components/LiveVideoPlayer.jsx
+++ src/components/LiveVideoPlayer.jsx
@@ -164,7 +164,7 @@ const YouTubePlayer = () => {
       {/* Enable Sound Button */}
       {isMuted && (
         <button
-          className="bg-green-500  text-white font-bold py-2 px-4 rounded"
+          className="bg-green-500  text-white font-bold py-2 px-4 mx-auto rounded"
           onClick={enableSound}
           style={{
             marginTop: "20px",
